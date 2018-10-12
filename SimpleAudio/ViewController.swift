43517d887a022af77fd95e42d12b40b26a534ab2
Victor Rosas <victorrosas@victors-macbook-pro.local>
2018-10-12 15:39:27 -0700

    Initial Commit

---
+++ SimpleAudio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleAudio
+//
+//  Created by Victor Rosas on 10/12/18.
+//  Copyright © 2018 Victor Rosas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
