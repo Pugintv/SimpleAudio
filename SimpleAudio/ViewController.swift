ae8342588584360e4255e8e9bc41dfee0e36cb5d
Victor Rosas <Victor Hugo Rosas>
2018-10-12 18:27:23 -0700

    Simple Audio Project, Play,Pause and Repeat

--- SimpleAudio/ViewController.swift
+++ SimpleAudio/ViewController.swift
@@ -7,14 +7,57 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf:URL.init(fileURLWithPath:Bundle.main.path(forResource: "sample", ofType: "mp3")!))
+            audioPlayer.prepareToPlay()
+            
+            //For Background play
+            var audioSession = AVAudioSession.sharedInstance()
+            
+            do{
+            try audioSession.setCategory(.playback, mode: .default, options: [])
+            }catch{
+                
+            }
+            
+        }catch{
+            print(error)
+        }
+        
     }
-
-
+    
+    @IBAction func pause(_ sender: Any) {
+        if audioPlayer.isPlaying {
+            audioPlayer.stop()
+        }else{
+            
+        }
+    }
+    
+    @IBAction func play(_ sender: Any) {
+        audioPlayer.play()
+    }
+    
+    
+    @IBAction func restart(_ sender: Any) {
+        if audioPlayer.isPlaying {
+            audioPlayer.currentTime = 0
+            audioPlayer.play()
+        }else{
+            audioPlayer.play()
+        }
+    }
+    
+    
 }
 
